d73adf516c51ff176ee83ec1a2f8ad77b6cf757f
225798 <mit225798@stud.mit.edu.au>
2024-06-02 18:23:38 +1000

    asd

--- src/main/java/com/github/Prabinnnnnnnnnn/Controller/CheckoutHandler.java
+++ src/main/java/com/github/Prabinnnnnnnnnn/Controller/CheckoutHandler.java
@@ -11,18 +11,19 @@ import com.github.Prabinnnnnnnnnn.models.Loan;
 import com.github.Prabinnnnnnnnnn.models.LoanStatus;
 import com.github.Prabinnnnnnnnnn.models.Patron;
 
-
+// controller for checkout
 public class CheckoutHandler {
     private final ArrayList<Loan> loans;
     private final ArrayList<Book> books;
     private final ArrayList<Patron> patrons;
-
+    
+    //constructor initialising lists
     public CheckoutHandler() {
         this.loans = new ArrayList<>();
         this.patrons = new ArrayList<>();
         this.books = new ArrayList<>();
     }
-
+    // verfies ID of patron returns boolean value
     public boolean verifyPatronForLoan(int ID) {
         for (Patron patron : patrons) {
             if (patron.getID() == ID) {
@@ -32,13 +33,13 @@ public class CheckoutHandler {
         return false;
     }
 
-
+    // checks out book with title and edition
     public void checkoutBook(int ID, String title, String edition) {
         if (!verifyPatronForLoan(ID)) {
             System.out.println("Patron not eligible for loan.");
             return;
         }
-
+        // loops through list until book is found
         Book book = null;
         for (Book b : books) {
             if (b.getTitle().equalsIgnoreCase(title) && b.getEdition().equalsIgnoreCase(edition)) {
@@ -51,7 +52,7 @@ public class CheckoutHandler {
             System.out.println("Book not found.");
             return;
         }
-
+        // finds available copies
         BookCopy availableCopy = null;
         for (BookCopy copy : book.getCopies()) {
             if (copy.getStatus().equals(BookCopyStatus.Available)) {
@@ -80,7 +81,7 @@ public class CheckoutHandler {
         availableCopy.setStatus(BookCopyStatus.OnLoan);
         loans.add(newLoan);
     }
-
+    // closes loan with ID
     public void closeLoan(int loanID) {
         for (Loan loan : loans) {
             if (loan.getLoanID() == loanID) {
@@ -89,6 +90,10 @@ public class CheckoutHandler {
             }
         }
     }
+    /*
+    * starting process of book return
+    * scanner reads input
+    */
 
     public void beginBookReturn() {
          try (Scanner scanner = new Scanner(System.in)) {
@@ -105,7 +110,7 @@ public class CheckoutHandler {
         }
     }
     
-
+    // returns book with title and edition
     public void returnBook(int ID, String title, String edition) {
         Book book = null;
         for (Book b : books) {
@@ -119,7 +124,7 @@ public class CheckoutHandler {
             System.out.println("Book not found.");
             return;
         }
-
+        // finds loaned copies
         BookCopy returnedCopy = null;
         for (BookCopy copy : book.getCopies()) {
             if (copy.getStatus().equals(BookCopyStatus.OnLoan)) {
@@ -132,7 +137,7 @@ public class CheckoutHandler {
             System.out.println("No on-loan copies found.");
             return;
         }
-
+        // updates of loan status and copy
         for (Loan loan : loans) {
             if (loan.getLoanID() == ID && loan.getStatus().equals(LoanStatus.Reserved)) {
                 loan.setStatus(LoanStatus.Returned);
@@ -141,7 +146,7 @@ public class CheckoutHandler {
             }
         }
     }
-
+    // ends process
     public void endBookReturn() {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Enter patron ID: ");
@@ -153,7 +158,7 @@ public class CheckoutHandler {
 
             System.out.print("Enter book edition: ");
             String edition = scanner.nextLine();
-
+            // finds Loan
             Loan foundLoan = null;
             for (Loan loan : loans) {
                 if (loan.getPatron().getID() == ID && loan.getStatus().equals(LoanStatus.Returned)){
@@ -166,9 +171,9 @@ public class CheckoutHandler {
                 System.out.println("No matching returned loan found.");
                 return;
             }
-   
+            // sets status
             foundLoan.setStatus(LoanStatus.Returned);
-   
+            
             Book book = null;
             for (Book b : books) {
                 if (b.getTitle().equalsIgnoreCase(title) && b.getEdition().equalsIgnoreCase(edition)) {
@@ -181,7 +186,7 @@ public class CheckoutHandler {
                 System.out.println("Book not found.");
                 return;
             }
-   
+            // sets status to available
             for (BookCopy copy : book.getCopies()) {
                 if (copy.getStatus().equals(BookCopyStatus.OnLoan)) {
                     copy.setStatus(BookCopyStatus.Available);
